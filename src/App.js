7fea0b7bfb6a89fbfe29409694276ccfc0b6456b
realizer <ipreet35232@gmail.com>
2024-12-20 11:50:02 +0530

    added login page with tailwind

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from "./components/Login/Login"
 import './css/global.css'
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          {/* <Route path='/login' element={<Signin />} /> */}
-          {/* <Route path='/create_new_account' element={<SignupForm />} /> */}
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    {<Route path='/login' element={<Login />} />}
+                    {/* <Route path='/create_new_account' element={<SignupForm />} /> */}
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
